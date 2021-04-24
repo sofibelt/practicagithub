5897807bfee94602ff802fef6f48c74c35705a39
Ana Sofia Beltran <anasobeltran@gmail.com>
2021-04-24 15:43:12 -0500

    cree el primer usuario

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-document.write("<h1> Hello world </h1>");
+class Usuario{
+  constructor(nombre,universidad){
+    this.nombre=nombre;
+    this.universidad=universidad;
+  }
+
+get  id(){
+    return this.nombre;
+  }
+
+get  idu(){
+    return this.univeridad;
+  }
+}
+
+
+
+const usuario1= new Usuario('ana','tecnologica');
+console.log(usuario1.id);
